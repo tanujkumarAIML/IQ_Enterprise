71e8d062bd303e99d2e514a4e7d85e01f513964d
tanujkumarAIML <tanujkumardhampur@gmail.com>
2026-06-24 07:25:09 +0530

    Fix Firebase Admin configuration

--- server/config/firebase.js
+++ server/config/firebase.js
@@ -1,10 +1,15 @@
 "use strict";
 
 const admin = require("firebase-admin");
-const serviceAccount = require("./firebase-service-account.json");
 
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-});
+if (!admin.apps.length) {
+  admin.initializeApp({
+    credential: admin.credential.cert({
+      projectId: process.env.FIREBASE_PROJECT_ID,
+      clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+      privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
+    }),
+  });
+}
 
 module.exports = admin;
